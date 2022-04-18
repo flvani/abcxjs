c46e69c46fb7b6da4e0c8c0365a55cf90f740581
Flavio Vani <flavio.vani@camara.leg.br>
2022-04-18 17:00:16 -0300

    6.17:tamanho dos simbolos de dedilhado

--- svg/glyphs.js
+++ svg/glyphs.js
@@ -13,7 +13,7 @@ if (!window.SVG.Glyphs )
 SVG.Glyphs = function () {
     
     var abc_glyphs = new ABCXJS.write.Glyphs();
-    var cn_const_scale = '0.06';
+    var cn_const_scale = '0.07'; // flavio - era 0.6 - não verifiquei se ficou bom em todos os casos.
     var cn_const_fill = '#f00';
 
     
