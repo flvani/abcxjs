7282e5872672619c72fdac9e7203b12530bafd1d
Flávio Vani <flavio.vani@gmail.com>
2024-04-07 21:51:13 -0300

    rebalance remove breaklines

--- parse/abc_rebalance.js
+++ parse/abc_rebalance.js
@@ -19,9 +19,9 @@ window.ABCXJS.parse.rebalance = function ( text ) {
     alert( 'A função rebalance é experimental!\n' +
             'O objetivo é alinhar a quantidade de compassos de\n' +
             'melodia e baixos por linha.\n' +
-            'Por hora, a diretiva %%barsperstaff é considerada\n' + 
+            'A diretiva %%barsperstaff é considerada\n' + 
             'e, na sua ausência, são distribuídos 6 compassos por linha.\n' + 
-            'Linebreaks são ignorados.\n' + 
+            'Linebreaks são removidos.\n' + 
             'Espera-se que as vozes V:1 e V:2 bass sejam bem definidas.'
         );
 
@@ -56,13 +56,18 @@ window.ABCXJS.parse.rebalance = function ( text ) {
     var liBass = -1;
     var lfBass = -1;
 
-    var split = function( text, maxbars ) {
+    var split = function( texto, maxbars, linebreak ) {
         var x0 = 0;
         var x1 = 0;
         var xi = 0;
         var cnt = 0;
         var newLines = [];
         var regex = /(?:[\:\|]|\[\|)+[\:\|\]]{0,}/; // identifica as barras de compasso
+
+        // Criando uma expressão regular usando a variável
+        var replaceLineBreakRegEx = new RegExp('(\\'+linebreak+')', 'gi');
+
+        var text = texto.replace( replaceLineBreakRegEx, '');
 
         var bar = text.substring(xi).match(regex);
 
@@ -115,7 +120,7 @@ window.ABCXJS.parse.rebalance = function ( text ) {
                lfBass = index;
                bassText += element.substring(0,commentX);
             } 
-            if( inTreble){
+            if( inTreble) {
                liTreble = liTreble === -1? index : liTreble;
                lfTreble = index;
                trebleText += element.substring(0,commentX);
@@ -124,8 +129,8 @@ window.ABCXJS.parse.rebalance = function ( text ) {
     }
 
 
-    var newTrebleLines = split( trebleText, barsperstaff );
-    var newBassLines = split( bassText, barsperstaff );
+    var newTrebleLines = split( trebleText, barsperstaff, linebreak );
+    var newBassLines = split( bassText, barsperstaff, linebreak );
 
     let nl = [
         ...lines.slice(0, liTreble),
